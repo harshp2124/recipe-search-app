dfd0324dfc5544ea23440dfe2b943db8d96c9d88
Harsh Patel <harsh@Harshs-MacBook-Air.local>
2020-07-04 16:26:00 +0530

    Data fetched

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Recipe from "./components/Recipe/Recipe";
+import { getRecipe } from "./api/index";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [query, setQuery] = useState("Milk");
+
+  const fetchRecipes = async () => {
+    const recipe = await getRecipe(query);
+    setRecipes(recipe);
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          className="search-bar"
+          type="text"
+          id="queryName"
+          placeholder={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="search-button" onClick={() => fetchRecipes()}>
+          Search
+        </button>
+      </div>
+      <div>
+        {recipes.length !== 0 ? (
+          recipes.map((recipe) => (
+            <Recipe
+              key={recipe.recipe.label}
+              title={recipe.recipe.label}
+              calories={recipe.recipe.calories}
+              image={recipe.recipe.image}
+            />
+          ))
+        ) : (
+          <p>Data not found!</p>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const APP_ID = "b8029f79";
+const APP_KEY = "897e9f4029e03c6ad29dd174e9c86645";
+
+export const getRecipe = async (q = "chicken") => {
+  try {
+    const URL = `https://api.edamam.com/search?q=${q}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    const {
+      data: { hits },
+    } = await axios.get(URL);
+    return hits;
+  } catch (error) {
+    console.log(error);
+  }
+};
